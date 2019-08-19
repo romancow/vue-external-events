b0290e25ee6ffc4f6aff5c0ed2660c4d3f2cccef
Sean Wilson <seancwilson@gmail.com>
2019-08-19 12:39:49 -0500

    Move plugin/mixin files to "plugins" folder

--- src/plugins/parent.ts
+++ src/plugins/parent.ts
@@ -1,6 +1,5 @@
 import Component from 'vue-class-component'
-import PassiveMixin from './passive'
-import * as Utilities from './utilities'
+import PassiveMixin, { ExternalEventsHandler } from './passive'
 
 type ExternalEventsMessageData = { channel: string, args?: any[] }
 type ExternalEvents = {
@@ -30,39 +29,33 @@ export default class ParentMixin extends PassiveMixin {
 
 	static targetOrigin = '*'
 
-	private [LISTENERS_KEY]: ExternalEvents
-
-	$onExternal(event: string | string[], callback: Function) {
-		Utilities.Array.ensure(event)
-			.forEach(channel => ExternalEvents.add(this[LISTENERS_KEY], channel, callback))
-		return this
-	}
-
-	$onceExternal(event: string, callback: Function) {
-		ExternalEvents.add(this[LISTENERS_KEY], event, callback, true)
-		return this
-	}
-
-	$offExternal(event?: string | string[], callback?: Function) {
-		const externalEvents = this[LISTENERS_KEY]
-		if (event == null)
-			externalEvents.channels = {}
-		else {
-			const { channels } = externalEvents
-			const all = (callback == null)
-			Utilities.Array.ensure(event).forEach(channel =>
-				channels[channel] = all ? [] :
-					channels[channel].filter(({callback: cb}) => cb != callback)
-			)
+	static $externalEventsHandler: ExternalEventsHandler = {
+		on(events: string[], callback: Function) {
+			events.forEach(channel => ExternalEvents.add(this[LISTENERS_KEY], channel, callback))
+		},
+		once(event: string, callback: Function) {
+			ExternalEvents.add(this[LISTENERS_KEY], event, callback, true)
+		},
+		off(events?: string[], callback?: Function) {
+			const externalEvents = this[LISTENERS_KEY]
+			if (events == null)
+				externalEvents.channels = {}
+			else {
+				const { channels } = externalEvents
+				const all = (callback == null)
+				events.forEach(channel =>
+					channels[channel] = all ? [] :
+						channels[channel].filter(({callback: cb}) => cb != callback)
+				)
+			}
+		},
+		emit(event: string, ...args: any[]) {
+			const message: ExternalEventsMessageData = { channel: event, args }
+			window.parent.postMessage(message, ParentMixin.targetOrigin)
 		}
-		return this
 	}
 
-	$emitExternal(event: string, ...args: any[]) {
-		const message: ExternalEventsMessageData = { channel: event, args }
-		window.parent.postMessage(message, ParentMixin.targetOrigin)
-		return this
-	}
+	private [LISTENERS_KEY]: ExternalEvents
 
 	created() {
 		const { listener } = this[LISTENERS_KEY] = {
