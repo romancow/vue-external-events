bdf004e7eb5838df4e71a41ce0afe30111389fff
Sean Wilson <seancwilson@gmail.com>
2019-08-27 12:37:32 -0500

    use rollup to create "main" and "browser" builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,29 @@
 // import pkg from './package.json';
 import resolve from 'rollup-plugin-node-resolve'
 
-export default {
-	input: 'mixins/parent.js',
-	external: ['electron', 'vue'],
+const cjs = ['electron', 'passive', 'parent'].map(name => ({
+	input: `es6/${name}.js`,
+	external: ['electron', 'vue', 'vue-class-component'],
 	context: 'window',
 	output: {
-		dir: "dist",
+		dir: "cjs",
 		name: "VueExternalEvents",
-		format: 'umd'
+		format: 'cjs'
 	},
 	plugins: [ resolve() ]
-}
+}))
+
+const iife = ['passive', 'parent'].map(name => ({
+	input: `es6/mixins/${name}.js`,
+	external: ['vue'],
+	context: 'window',
+	output: {
+		dir: "iife",
+		name: "VueExternalEvents",
+		format: 'iife',
+		globals: { vue: 'Vue' }
+	},
+	plugins: [ resolve() ]
+}))
+
+export default [...cjs, ...iife]
